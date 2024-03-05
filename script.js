3da228763b7bc889bb93218816f549071629b1d1
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-05 18:40:05 +0600

    mark btn added in section discuss

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const allPost = document.querySelectorAll('.post-before-mark');
+const allMarkBtn = document.querySelectorAll('.mark-btn');
 const markAsRead = document.getElementById('markCount');
 
-allPost.forEach((post) => {
+const postTitle1 = document.getElementsByClassName('.post-title1');
+const postViewer1 = document.getElementsByClassName('.post-viewer1');
+
+const postTitle2 = document.getElementsByClassName('.post-title2');
+const postViewer2 = document.getElementsByClassName('.post-viewer2');
+
+const markedPostContaier = document.getElementById('#markedPostContainer');
+
+allMarkBtn.forEach((post) => {
     post.addEventListener('click', function () {
         let count = 0;
         count = count + 1;
-        const totalPost=parseInt(markAsRead.innerText)+count;
-        markAsRead.innerText=totalPost;
+        const totalPost = parseInt(markAsRead.innerText) + count;
+        markAsRead.innerText = totalPost;
+
+        const li = document.createElement('li');
+        const p1 = document.createElement('p');
+        p1.innerText = postTitle1;
+        const img = document.createElement('img')
+        const p2 = document.createElement('p');
+        p2.innerText = postViewer1;
+        li.appendChild(p1);
+        li.appendChild(img);
+        li.appendChild(p2);
+        console.log(li);
+        markedPostContaier.appendChild(li);
     })
 })
