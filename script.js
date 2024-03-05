c4aa0477eed6f3635dacb630f5e4dc7224aaa1c3
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-05 19:58:02 +0600

    section discuss some js added

--- script.js
+++ script.js
@@ -20,10 +20,20 @@ allMarkBtn.forEach((post) => {
         const div=document.createElement('div');
         const p1=document.createElement('p');
         p1.innerText=postTitleText;
+        let img=document.createElement('img');
+        img.src = './images/eye.png'
         const p2=document.createElement('p');
         p2.innerText=postViewNum;
         div.appendChild(p1);
+        div.appendChild(img);
         div.appendChild(p2);
+        div.classList.add('flex');
+        div.classList.add('justify-between');
+        div.classList.add('items-center');
+        div.classList.add('bg-white');
+        div.classList.add('text-black');
+        div.classList.add('p-2');
+        div.classList.add('rounded-xl');
         markedPostContaier.appendChild(div);
     })
 })
