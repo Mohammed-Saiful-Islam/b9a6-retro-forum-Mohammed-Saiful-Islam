c9ae99c56a13d70c999778c8df6464ee15afafed
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-05 19:26:47 +0600

    section discuss section marked post some js added

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const markAsRead = document.getElementById('markCount');
 
 const postTitle = document.getElementById('post-title');
 const postView = document.getElementById('post-view');
+const viewEye=document.getElementById('eye');
 
-const markedPostContaier = document.getElementById('#markedPostContainer');
+const markedPostContaier = document.getElementById('markedPostContainer');
 
 allMarkBtn.forEach((post) => {
     post.addEventListener('click', function () {
@@ -15,6 +16,14 @@ allMarkBtn.forEach((post) => {
 
         const postTitleText=postTitle.innerText;
         const postViewNum=postView.innerText;
-        
+
+        const div=document.createElement('div');
+        const p1=document.createElement('p');
+        p1.innerText=postTitleText;
+        const p2=document.createElement('p');
+        p2.innerText=postViewNum;
+        div.appendChild(p1);
+        div.appendChild(p2);
+        markedPostContaier.appendChild(div);
     })
 })
