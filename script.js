0fbed2085f6720d59e0af89241bc23e39296f016
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-05 19:07:55 +0600

    section discuss some js edited

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const allMarkBtn = document.querySelectorAll('.mark-btn');
 const markAsRead = document.getElementById('markCount');
 
-const postTitle1 = document.getElementsByClassName('.post-title1');
-const postViewer1 = document.getElementsByClassName('.post-viewer1');
-
-const postTitle2 = document.getElementsByClassName('.post-title2');
-const postViewer2 = document.getElementsByClassName('.post-viewer2');
+const postTitle = document.getElementById('post-title');
+const postView = document.getElementById('post-view');
 
 const markedPostContaier = document.getElementById('#markedPostContainer');
 
@@ -16,16 +13,8 @@ allMarkBtn.forEach((post) => {
         const totalPost = parseInt(markAsRead.innerText) + count;
         markAsRead.innerText = totalPost;
 
-        const li = document.createElement('li');
-        const p1 = document.createElement('p');
-        p1.innerText = postTitle1;
-        const img = document.createElement('img')
-        const p2 = document.createElement('p');
-        p2.innerText = postViewer1;
-        li.appendChild(p1);
-        li.appendChild(img);
-        li.appendChild(p2);
-        console.log(li);
-        markedPostContaier.appendChild(li);
+        const postTitleText=postTitle.innerText;
+        const postViewNum=postView.innerText;
+        
     })
 })
