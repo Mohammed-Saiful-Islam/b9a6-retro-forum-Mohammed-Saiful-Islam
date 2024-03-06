3d24f655b7d3f7cfd79796c5e90aeebaec156390
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-06 14:57:15 +0600

    section discuss some js added and edited

--- script.js
+++ script.js
@@ -83,7 +83,39 @@ const loadPosts = async () => {
                             </div>
         `
         postBeforeMarkContainer.appendChild(div);
+    });
+    const allMarkBtn = document.querySelectorAll('.mark-btn');
+    allMarkBtn.forEach((post) => {
+        post.addEventListener('click', function () {
+            let count = 0;
+            count = count + 1;
+            const totalPost = parseInt(markAsRead.innerText) + count;
+            markAsRead.innerText = totalPost;
+    
+            const postTitleText = postTitle.innerText;
+            const postViewNum = postView.innerText;
+    
+            const div = document.createElement('div');
+            const p1 = document.createElement('p');
+            p1.innerText = postTitleText;
+            let img = document.createElement('img');
+            img.src = './images/eye.png'
+            const p2 = document.createElement('p');
+            p2.innerText = postViewNum;
+            div.appendChild(p1);
+            div.appendChild(img);
+            div.appendChild(p2);
+            div.classList.add('flex');
+            div.classList.add('justify-between');
+            div.classList.add('items-center');
+            div.classList.add('bg-white');
+            div.classList.add('text-black');
+            div.classList.add('p-2');
+            div.classList.add('rounded-xl');
+            markedPostContaier.appendChild(div);
+        })
     })
+    
 }
 loadPosts();
 
